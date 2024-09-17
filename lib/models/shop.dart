7da8a2f9c8fbe9aa84c095e5d9376ca0769c3376
Crecendoed <cwyler831@gmail.com>
2024-09-17 10:44:06 -0500

    Edit to main and shop model

--- lib/models/shop.dart
+++ lib/models/shop.dart
@@ -1,11 +1,16 @@
+import 'package:e_commerce_app_with_cart/models/product.dart';
+
 class Shop{
   
   // Products for Sale
-  
+  late List<Product> products;
 
   // User cart
+  late List<Product> cart = [
+    
+  ];
 
-  // Get Product Lis
+  // Get Product List
 
   // Get User Cart
 
